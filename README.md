ea633e67a1dd3b7069b54a90b74d047d0c142d16
Kas Buunk <kasbuunk@icloud.com>
2022-11-22 21:53:10 +0100

    feat: initial commit: implement simple http server

---
+++ README.md
@@ -0,0 +1,3 @@
+# Building a simple http server in Rust
+
+This is an example project to experiment with thread pools and building a basic HTTP server on top of Rust's standard crate for TCP connections.
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use http::ThreadPool;
+use std::thread;
+use std::time::Duration;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+fn main() {
+    init_http_server();
+}
+
+fn init_http_server() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let body = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        body.len(),
+        body,
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
